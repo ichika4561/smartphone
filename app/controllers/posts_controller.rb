f1ad8b1e81e817c28edce02daa8e87f1d66db107
EC2 Default User <ec2-user@ip-172-31-28-213.us-east-2.compute.internal>
2018-12-02 08:18:08 +0000

    flash

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,22 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post =
-  Post.new
+    @post = Post.new
   end
 
   def edit
   end
+  
+  def create
+    post = Post.new(post_params)
+    post.save!
+    redirect_to posts_url, notise: "投稿「#{post.name}」を登録しました。"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :description)
+  end
+
 end
