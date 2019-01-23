1de8975959631bf9307b149d9656c3477e58ab51
椎木千翔 <ichika4561@gmail.com>
2019-01-23 17:10:29 +0900

    before ligin

--- test/controllers/sessions_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get settions_new_url
-    assert_response :success
-  end
-
-end
