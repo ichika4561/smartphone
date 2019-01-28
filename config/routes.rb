30ab56d72ed9a8c6ce2914fb09a4c3e869d11929
椎木千翔 <ichika4561@gmail.com>
2019-01-28 17:34:24 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  get 'home/index'
+
+  get '/',to: 'home#index'
   namespace :admin do
-  resources :users
+   resources :users
   end
+
   get'/login',to: 'sessions#new'
   post'/login',to: 'sessions#create'
   delete'/logout',to: 'sessions#destroy'
   root to: 'top#show'
+  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get 'top/show'
   #ログイン後にはsessions#new
   #root to: 'posts#index'
   #↑トップ画面に行ったら自動的に投稿一覧に飛ぶ設定。
-  resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
